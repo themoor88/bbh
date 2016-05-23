57e7b197cca0393c15a4598ee9d141adc85c74e7
WajidKagzi <wjkagzi@gmail.com>
2016-05-23 17:04:35 -0400

    Proposed solution modal and creation

--- app/controllers/dashboard/proposed_solutions_controller.rb
+++ app/controllers/dashboard/proposed_solutions_controller.rb
@@ -3,19 +3,20 @@ class Dashboard::ProposedSolutionsController < ApplicationController
   # GET /dashboard/proposed_solutions/new
   def new
     @proposed_solution = current_tech_provider.proposed_solutions.new
+    @campaign = Campaign.find(params[:campaign_id])
+    render layout: false
   end
 
   # POST /dashboard/proposed_solutions
   def create
     @proposed_solution = current_tech_provider.proposed_solutions.new(proposed_solution_params)
+    @proposed_solution.campaign = Campaign.find(params[:proposed_solution][:campaign_id])
 
     respond_to do |format|
       if @proposed_solution.save
-        format.html { redirect_to dashboard_proposed_solutions_path, notice: 'Proposed solution was successfully created.' }
-        format.json { render :show, status: :created, location: @proposed_solution }
+        format.html { redirect_to campaigns_path, notice: 'Proposed solution was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @proposed_solution.errors, status: :unprocessable_entity }
       end
     end
   end
