f6ca4a94014c278f283858e417f70f2cee3f03e0
WajidKagzi <wjkagzi@gmail.com>
2016-05-23 11:03:37 -0400

    Cleaning up campaigns views and controllers

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class CampaignsController < ApplicationController
-  before_action :set_campaign, only: [:show, :edit, :update, :destroy]
+  before_action :set_campaign, only: [:show]
 
   # GET /campaigns
   def index
@@ -16,10 +16,5 @@ class CampaignsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_campaign
     @campaign = Campaign.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def campaign_params
-    params.require(:campaign).permit(:name, :description, :state, :expires_at)
   end
 end
