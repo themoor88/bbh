057e6d47ff21f9cec7c95284ac0a8742ffc7a7d9
WajidKagzi <wjkagzi@gmail.com>
2016-05-22 20:51:54 -0400

    Modifying routes and views to show appropriate fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,33 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admins, skip: [:registrations], controllers: {
+    sessions: 'admins/devise/sessions'
+  }
+
+  devise_for :tech_providers, controllers: {
+    registrations: 'tech_providers/devise/registrations',
+    sessions: 'tech_providers/devise/sessions'
+  }
+
+  devise_for :tech_seekers, controllers: {
+    registrations: 'tech_seekers/devise/registrations',
+    sessions: 'tech_seekers/devise/sessions'
+  }
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'       # needs to be AFTER "devise_for :admins" otherwise the admin routes will be messed up
+
   root to: 'visitors#index'
-  devise_for :users
-  resources :users
+  resources :proposed_solutions, only: [:index, :show]
+  resources :campaigns, only: [:index, :show, :new]
+
+  authenticate :tech_provider do
+    namespace :dashboard do
+      resources :proposed_solutions
+    end
+  end
+
+  authenticate :tech_seeker do
+    namespace :dashboard do
+      resources :campaigns
+    end
+  end
 end
