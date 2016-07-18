3f11e89f6d32cc886b7effa14e5127190ab2dada
Wajid Kagzi <wjkagzi@gmail.com>
2016-07-17 22:52:25 -0400

    Create better view for campaign listings and precompile assets

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -63,8 +63,13 @@ $(function () {
     $('[data-countdown]').each(function() {
       var $this = $(this), finalDate = $(this).data('countdown');
       $this.countdown(finalDate, function(event) {
-        $this.html(event.strftime('%D days %H:%M:%S'));
+        $this.html(event.strftime('%ddays %Hhrs %Mmin'));
       });
+    });
+
+
+    $('.campaign-panel .panel-body').on('click', function() {
+      window.location.href = '/campaigns/' + $(this).data('campaign-id');
     });
   }
 });
